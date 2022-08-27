3f7ce538b54c1d22cf592bc2e0cf36c8249855ed
Tyler Holewinski <tyler.holewinski@bryx.com>
2022-08-26 22:12:44 -0500

    inital commit

---
+++ content-script.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(document.location.search);
+
+const cartGuid = params.get('cart');
+
+window.localStorage.setItem('toast-oo-restaurant', JSON.stringify({ '7fb7d7c2-7204-4fbe-ae03-ce2324ecab68': { cartGuid } }));
+
+console.log(cartGuid);
+
+
+if (params.get('rl') == '1') {
+  params.set('rl', '0');
+  window.location.search = params.toString();
+}
+
+
